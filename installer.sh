3158757dbeb6a0d705226436eeee9ffa784dc7b9
Evan Nordquist <nordquist.evan@gmail.com>
2021-10-09 16:22:11 -0400

    Minimum Viable Product, tested on a Mac.

---
+++ installer.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+clear;
+ECHO "Installing Mac Compatible TKinter library -tkmacosx";
+pip3 install tkmacosx --disable-pip-version-check;
+
+ECHO "#############################";
+ECHO "Making launcher.sh an executable file";
+
+chmod +x launcher.sh;
+ECHO "Finished making the launcher executable.";
+
+ECHO "#############################";
+ECHO "You will want to go to Terminal>Preferences";
+ECHO "Select the 'Shell' tab";
+ECHO "Select 'Close if the shell exited cleanly' from drop-down menu";
+
+
---
+++ launcher.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~/Desktop/Mac_User_Agreement;
+python3 ./mac_tkVersion.py;
+exit;
